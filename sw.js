067467dca177f3e2c484565b155679c07e468288
gamache61 <truckman.remi@gmail.com>
2025-12-16 23:01:56 -0800

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-const CACHE_NAME = 'helix-v50-stable';
-const urlsToCache = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))); });
-self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); caches.keys().then(k => Promise.all(k.map(n => n !== CACHE_NAME && caches.delete(n)))); });
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
