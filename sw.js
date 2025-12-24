4be56856a10e30867ab5c10506bc676d6e3cf887
gamache61 <truckman.remi@gmail.com>
2025-12-24 08:56:24 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+// HELIX SERVICE WORKER - v47.5
+const CACHE_NAME = 'helix-cache-v47.5'; 
+const assetsToCache = [
+  './',
+  'index.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assetsToCache))
+  );
+  // Force the new service worker to activate immediately
+  self.skipWaiting(); 
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Clearing old Helix cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
